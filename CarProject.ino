0ffcbfc54081309b4410e5cede3b3e6220a7a4ea
Andrew <96ahill@gmail.com>
2018-04-18 18:03:35 -0600

    Added 90 degree turns to car.

--- CarProject.ino
+++ CarProject.ino
@@ -6,8 +6,9 @@ The range readings are in units of mm. */
 #include <Wire.h>
 #include <VL6180X.h>
 
-#define defaultSpeed 85
-#define correctionSpeed 110
+#define defaultSpeed 75 // was 85
+#define correctionSpeed 100 // was 110
+#define turnSpeed 150
 
 VL6180X sensor1;
 VL6180X sensor2;
@@ -69,54 +70,80 @@ void setup()
 //front sensor = 1
 //lhs sensor = 2
 //rhs sensor = 3
-void loop() 
+void loop(){
+  delay(3000);
+  rotateRight();
+}
+void loop_old() 
 { 
   s1_val = sensor1.readRangeSingleMillimeters();
   s2_val = sensor2.readRangeSingleMillimeters();
   s3_val = sensor3.readRangeSingleMillimeters();
-  printLaserReadings();
-  Serial.println( (int)s2_val - (int)s3_val);
+  //printLaserReadings();
+  //Serial.println( (int)s2_val - (int)s3_val);
   if(s1_val > 80 && s2_val > 15 && s3_val > 15){
+    Serial.println("Go straight");
     drive_straight();
   }
   else if(s2_val <= 15){
+    Serial.println("Begin right correction:");
     //backup and slightly turn right
+    Serial.println("Stop car");
     car_stop();
     delay(500);
+    Serial.println("Back up right");
     while(sensor2.readRangeSingleMillimeters() <= 25){
+      Serial.println("In back-up right loop");
       backward();
     }
+    Serial.println("Slight turn right");
+    smallPivotRight();
+    Serial.println("Finished back up right. Stop.");
     car_stop();
   }
   else if(s3_val <= 15){
+    Serial.println("Begin left correction:");
     //backup and slightly turn left
+    Serial.println("Stop car");
     car_stop();
     delay(500);
-    while(sensor2.readRangeSingleMillimeters() <= 25){
+    Serial.println("Back up left");
+    // Back up to release car from wall.
+    while(sensor3.readRangeSingleMillimeters() <= 25){
+      Serial.println("In back-up left loop");
       backward();
     }
+    Serial.println("Slight turn left");
+    smallPivotLeft();
+    Serial.println("Finished back up left. Stop.");
     car_stop();
   }
   else{
+    Serial.println("Stopping car.");
     car_stop();
   }
   
-  delay(100);
+  //delay(100);
 }
 
 void drive_straight(){
+    digitalWrite(8, LOW);
+    digitalWrite(11, HIGH);
+    digitalWrite(12, HIGH);
+    digitalWrite(13, LOW);  
     if( (int)s2_val - (int)s3_val < -8){
     //slight turn right
+    Serial.println("Slight correction right.");
     analogWrite(10, correctionSpeed);
     analogWrite(9, defaultSpeed);
-     
-    Serial.println("Value was negative");
-    Serial.println((int)s2_val - (int)s3_val);
+    //Serial.println("Value was negative");
+    //Serial.println((int)s2_val - (int)s3_val);
   }
   else if( (int)s2_val - (int)s3_val > 8){
     //slight turn left
-    Serial.println("Value was positive");
-    Serial.println((int)s2_val - (int)s3_val);
+    //Serial.println("Value was positive");
+    //Serial.println((int)s2_val - (int)s3_val);
+    Serial.println("Slight correction left");
     analogWrite(9, correctionSpeed);
     analogWrite(10, defaultSpeed);
   }
@@ -166,4 +193,47 @@ void printLaserReadings(){
   Serial.println();
 }
 
+void smallPivotRight(){
+  digitalWrite(8, HIGH);
+  digitalWrite(11, LOW);
+  digitalWrite(12, HIGH);
+  digitalWrite(13, LOW);
+  analogWrite(9, defaultSpeed);
+  analogWrite(10, defaultSpeed);
+  delay(200);
+  car_stop();
+}
+
+void smallPivotLeft(){
+  digitalWrite(8, LOW);
+  digitalWrite(11, HIGH);
+  digitalWrite(12, LOW);
+  digitalWrite(13, HIGH);
+  analogWrite(9, defaultSpeed);
+  analogWrite(10, defaultSpeed);
+  delay(200);
+  car_stop();
+}
+
+void rotateRight(){
+  digitalWrite(8,HIGH);
+  digitalWrite(11,LOW);
+  digitalWrite(12, HIGH);
+  digitalWrite(13,LOW);
+  analogWrite(9, turnSpeed);
+  analogWrite(10, turnSpeed);
+  delay(300);
+  car_stop();
+}
+
+void rotateLeft(){
+   digitalWrite(8, LOW);
+  digitalWrite(11, HIGH);
+  digitalWrite(12, LOW);
+  digitalWrite(13, HIGH);
+  analogWrite(9, defaultSpeed);
+  analogWrite(10, defaultSpeed);
+  delay(1000);
+  car_stop();
+}
 
